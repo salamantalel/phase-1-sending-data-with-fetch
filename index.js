0e709a82da3d98a4e89f53da271fc085042f95e1
salamantalel <nmoinkett@gmail.com>
2025-06-30 20:05:45 +0300

    Complete sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+  const formData = {
+    name: name,
+    email: email,
+  };
+
+  const configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+
+  return fetch("http://localhost:3000/users", configObj)
+    .then((response) => response.json())
+    .then((object) => {
+      //  This part adds the new ID to the DOM
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = `New user ID: ${object.id}`;
+      body.appendChild(p);
+    })
+    .catch((error) => {
+      const body = document.querySelector("body");
+      const p = document.createElement("p");
+      p.textContent = error.message;
+      body.appendChild(p);
+    });
+}
